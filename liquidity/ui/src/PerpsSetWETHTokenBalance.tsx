7975904f88aad96b542decbdf5baa1d34532e34b
Volodymyr Derunov <vderunov@gmail.com>
2024-09-27 01:21:19 +0300

    Remove usePerpsDeposit and get from react sdk (#25)
    * Remove usePerpsDeposit and get from react sdk
    
    * Switch from usePerpsDeposit to useWethDeposit

--- liquidity/ui/src/PerpsSetWETHTokenBalance.tsx
+++ liquidity/ui/src/PerpsSetWETHTokenBalance.tsx
@@ -1,22 +1,28 @@
 import { Box, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Text } from '@chakra-ui/react';
-import { useImportExtras } from '@synthetixio/react-sdk';
+import { useImportExtras, useSynthetix, useWethDeposit } from '@synthetixio/react-sdk';
+import { useQueryClient } from '@tanstack/react-query';
 import { useConnectWallet } from '@web3-onboard/react';
 import React from 'react';
 import { parseAmount } from './parseAmount';
 import { renderAmount } from './renderAmount';
 import { useCollateralTokens } from './useCollateralTokens';
 import { useEthBalance } from './useEthBalance';
-import { usePerpsDeposit } from './usePerpsDeposit';
+import { usePerpsSelectedAccountId } from './usePerpsSelectedAccountId';
+import { useProvider } from './useProvider';
 import { useTokenBalance } from './useTokenBalance';
 
 export function PerpsSetWETHTokenBalance() {
   const [{ wallet }] = useConnectWallet();
   const walletAddress = wallet?.accounts?.[0]?.address;
 
+  const { chainId } = useSynthetix();
+  const provider = useProvider();
+  const perpsAccountId = usePerpsSelectedAccountId();
   const { data: extras } = useImportExtras();
   const collateralTokens = useCollateralTokens();
   const tokenWETH = extras && collateralTokens?.find((token) => token.address === extras.weth_address);
 
+  const queryClient = useQueryClient();
   const currentEthBalance = useEthBalance();
   const { data: currentBalance } = useTokenBalance({
     ownerAddress: walletAddress,
@@ -25,10 +31,20 @@ export function PerpsSetWETHTokenBalance() {
 
   const [value, setValue] = React.useState('');
   const parsedAmount = parseAmount(value, 18);
+  const wethDeposit = useWethDeposit({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [chainId, 'Balance', { tokenAddress: tokenWETH?.address, ownerAddress: walletAddress }],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [chainId, 'EthBalance', { ownerAddress: walletAddress }],
+      });
 
-  const perpsDeposit = usePerpsDeposit({
-    onSuccess: () => setValue(''),
-    tokenAddress: tokenWETH?.address,
+      setValue('');
+    },
+    provider,
+    walletAddress,
+    perpsAccountId,
   });
 
   return (
@@ -42,10 +58,10 @@ export function PerpsSetWETHTokenBalance() {
         as="form"
         onSubmit={(e) => {
           e.preventDefault();
-          perpsDeposit.mutate(parsedAmount);
+          wethDeposit.mutate(parsedAmount);
         }}
       >
-        <FormControl isInvalid={perpsDeposit.isError}>
+        <FormControl isInvalid={wethDeposit.isError}>
           <FormLabel fontSize="3xl">Convert ETH to WETH (Wrapped ETH)</FormLabel>
           <Text mb="2">
             Current WETH Balance:{' '}
@@ -55,15 +71,15 @@ export function PerpsSetWETHTokenBalance() {
             Current ETH Balance: <b>{renderAmount(currentEthBalance.data, { symbol: 'ETH', decimals: 18 })}</b>
           </Text>
           <Input required placeholder="Enter amount" value={value} onChange={(e) => setValue(e.target.value)} />
-          {perpsDeposit.isError ? (
-            <FormErrorMessage>{perpsDeposit.error?.message}</FormErrorMessage>
+          {wethDeposit.isError ? (
+            <FormErrorMessage>{wethDeposit.error?.message}</FormErrorMessage>
           ) : (
             <FormHelperText>
               Max: <b>{currentEthBalance ? renderAmount(currentEthBalance.data, { symbol: 'ETH', decimals: 18 }) : null}</b>
             </FormHelperText>
           )}
         </FormControl>
-        <Button type="submit" mt="5%" isLoading={perpsDeposit.isPending}>
+        <Button type="submit" mt="5%" isLoading={wethDeposit.isPending}>
           Convert ETH to WETH
         </Button>
       </Box>
